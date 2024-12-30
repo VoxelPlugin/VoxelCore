eaaa46b31f92b52a4c5ba1f559e3c108782cee44
Victor Careil <victor.careil@voxelplugin.com>
2024-12-30 02:50:05 +0000

    Fix voxel scalar & vector parameters derivatives

---
+++ Source/VoxelCore/Private/VoxelMinimal/Utilities/VoxelMaterialUtilities.cpp
@@ -0,0 +1,25 @@
+﻿// Copyright Voxel Plugin SAS. All Rights Reserved.
+
+#include "VoxelMinimal.h"
+#include "Materials/HLSLMaterialTranslator.h"
+
+int32 FVoxelUtilities::ZeroDerivative(
+	FMaterialCompiler& Compiler,
+	const int32 Index)
+{
+	if (Index == -1)
+	{
+		return -1;
+	}
+
+	struct FHack : FHLSLMaterialTranslator
+	{
+		void Fix(const int32 Index) const
+		{
+			(*CurrentScopeChunks)[Index].DerivativeStatus = EDerivativeStatus::Zero;
+		}
+	};
+	static_cast<FHack&>(Compiler).Fix(Index);
+
+	return Index;
+}
---
+++ Source/VoxelCore/Public/VoxelMinimal/Utilities/VoxelMaterialUtilities.h
@@ -0,0 +1,14 @@
+﻿// Copyright Voxel Plugin SAS. All Rights Reserved.
+
+#pragma once
+
+#include "VoxelCoreMinimal.h"
+
+class FMaterialCompiler;
+
+namespace FVoxelUtilities
+{
+	VOXELCORE_API int32 ZeroDerivative(
+		FMaterialCompiler& Compiler,
+		int32 Index);
+};
