eebb8d1f3a898202b0efa7c234e89254aa3afd2d
Victor Careil <victor.careil@voxelplugin.com>
2025-01-28 03:04:33 +0000

    MegaMaterial improvements

--- Source/VoxelCore/Private/VoxelMinimal/Utilities/VoxelMaterialUtilities.cpp
+++ Source/VoxelCore/Private/VoxelMinimal/Utilities/VoxelMaterialUtilities.cpp
@@ -1,7 +1,11 @@
 ﻿// Copyright Voxel Plugin SAS. All Rights Reserved.
 
 #include "VoxelMinimal.h"
+#if WITH_EDITOR
+#include "MaterialEditingLibrary.h"
+#include "Materials/MaterialFunction.h"
 #include "Materials/HLSLMaterialTranslator.h"
+#endif
 
 #if WITH_EDITOR
 int32 FVoxelUtilities::ZeroDerivative(
@@ -23,5 +27,35 @@ int32 FVoxelUtilities::ZeroDerivative(
 	static_cast<FHack&>(Compiler).Fix(Index);
 
 	return Index;
+}
+
+UMaterialExpression& FVoxelUtilities::CreateMaterialExpression(
+	UObject& Outer,
+	TSubclassOf<UMaterialExpression> ExpressionClass)
+{
+	if (UMaterial* Material = Cast<UMaterial>(&Outer))
+	{
+		UMaterialExpression* Expression = UMaterialEditingLibrary::CreateMaterialExpression(
+			Material,
+			ExpressionClass);
+
+		check(Expression);
+
+		return *Expression;
+	}
+
+	if (UMaterialFunction* Function = Cast<UMaterialFunction>(&Outer))
+	{
+		UMaterialExpression* Expression = UMaterialEditingLibrary::CreateMaterialExpressionInFunction(
+			Function,
+			ExpressionClass);
+
+		check(Expression);
+
+		return *Expression;
+	}
+
+	check(false);
+	return *GetMutableDefault<UMaterialExpression>();
 }
 #endif
--- Source/VoxelCore/Public/VoxelMinimal/Utilities/VoxelMaterialUtilities.h
+++ Source/VoxelCore/Public/VoxelMinimal/Utilities/VoxelMaterialUtilities.h
@@ -5,6 +5,8 @@
 #include "VoxelCoreMinimal.h"
 
 class FMaterialCompiler;
+class UMaterialFunction;
+class UMaterialExpression;
 
 namespace FVoxelUtilities
 {
@@ -12,5 +14,15 @@ namespace FVoxelUtilities
 	VOXELCORE_API int32 ZeroDerivative(
 		FMaterialCompiler& Compiler,
 		int32 Index);
+
+	VOXELCORE_API UMaterialExpression& CreateMaterialExpression(
+		UObject& Outer,
+		TSubclassOf<UMaterialExpression> ExpressionClass);
+
+	template<typename T, typename = std::enable_if_t<TIsDerivedFrom<T, UMaterialExpression>::Value>>
+	T& CreateMaterialExpression(UObject& Outer)
+	{
+		return *CastChecked<T>(&FVoxelUtilities::CreateMaterialExpression(Outer, T::StaticClass()));
+	}
 #endif
 };
