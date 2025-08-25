bc7dd99c9e31a7d4a5267335e9cc6fb54adf5de5
Voxel Plugin Builder <builder@voxelplugin.com>
2025-08-25 11:57:08 +0000

    Rename Use with Lidar material property

--- Source/VoxelCore/Private/VoxelMaterialUsage.cpp
+++ Source/VoxelCore/Private/VoxelMaterialUsage.cpp
@@ -8,11 +8,11 @@
 VOXEL_RUN_ON_STARTUP_EDITOR()
 {
 	FProperty& Property = FindFPropertyChecked(UMaterial, bUsedWithLidarPointCloud);
-	Property.SetMetaData("DisplayName", "Use with Lidar or Voxel Plugin");
+	Property.SetMetaData("DisplayName", "Use with Voxel Plugin or Lidar");
 
 	Property.SetMetaData("Tooltip",
-		"Indicates that the material and its instances can be use with LiDAR Point Clouds & with Voxel Plugin.\n"
-		"This will result in the shaders required to support LiDAR and Voxel Plugin being compiled which will increase shader compile time and memory usage.");
+		"Indicates that the material and its instances can be use with Voxel Plugin & with LiDAR Point Clouds.\n"
+		"This will result in the shaders required to support Voxel Plugin and LiDAR being compiled which will increase shader compile time and memory usage.");
 }
 #endif
 
