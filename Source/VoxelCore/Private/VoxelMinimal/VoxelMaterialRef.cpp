5be84029af228683a6c7c3374282b0103b25af85
Voxel Plugin Builder <builder@voxelplugin.com>
2025-10-15 13:49:31 +0000

    Add caching for texture streaming

--- Source/VoxelCore/Private/VoxelMinimal/VoxelMaterialRef.cpp
+++ Source/VoxelCore/Private/VoxelMinimal/VoxelMaterialRef.cpp
@@ -3,6 +3,7 @@
 #include "VoxelMinimal.h"
 #include "MaterialDomain.h"
 #include "Materials/Material.h"
+#include "Engine/StreamableRenderAsset.h"
 #include "Materials/MaterialInstanceDynamic.h"
 
 DEFINE_VOXEL_INSTANCE_COUNTER(FVoxelMaterialRef);
@@ -69,6 +70,67 @@ TSharedRef<FVoxelMaterialRef> FVoxelMaterialRef::Make(UMaterialInterface* Materi
 	GVoxelMaterialRefManager->MaterialRefs.Add(MaterialRef);
 
 	return MaterialRef;
+}
+
+void FVoxelMaterialRef::GetStreamingRenderAssetInfo(
+	FStreamingTextureLevelContext& LevelContext,
+	const FBoxSphereBounds& Bounds,
+	const float ComponentScale,
+	TArray<FStreamingRenderAssetPrimitiveInfo>& OutStreamingRenderAssets)
+{
+	VOXEL_FUNCTION_COUNTER();
+
+	// Ensure that we have correct textures for current Feature and Quality levels
+	if (INLINE_LAMBDA
+		{
+			if (!FeatureLevel.IsSet() ||
+				!QualityLevel.IsSet())
+			{
+				return true;
+			}
+
+			return
+				FeatureLevel.GetValue() != LevelContext.GetFeatureLevel() ||
+				QualityLevel.GetValue() != LevelContext.GetQualityLevel();
+		})
+	{
+		VOXEL_SCOPE_COUNTER("Build texture refs [" + WeakMaterial.GetPathName() + "]");
+
+		FeatureLevel = LevelContext.GetFeatureLevel();
+		QualityLevel = LevelContext.GetQualityLevel();
+		TextureStreamingRefs.Reset();
+
+		static const FMeshUVChannelInfo UVChannelData(1.f);
+
+		FPrimitiveMaterialInfo MaterialData;
+		MaterialData.PackedRelativeBox = PackedRelativeBox_Identity;
+		MaterialData.UVChannelData = &UVChannelData;
+		MaterialData.Material = GetMaterial();
+
+		LevelContext.ProcessMaterial(
+			Bounds,
+			MaterialData,
+			ComponentScale,
+			OutStreamingRenderAssets,
+			false);
+
+		for (const FStreamingRenderAssetPrimitiveInfo& Info : OutStreamingRenderAssets)
+		{
+			FVoxelTextureStreamingRef& Ref = TextureStreamingRefs.Emplace_GetRef();
+			Ref.Texture = Info.RenderAsset;
+			Ref.TexelFactor = Info.TexelFactor / ComponentScale;
+		}
+		return;
+	}
+
+	for (const FVoxelTextureStreamingRef& Ref : TextureStreamingRefs)
+	{
+		FStreamingRenderAssetPrimitiveInfo& Info = OutStreamingRenderAssets.AddDefaulted_GetRef();
+		Info.RenderAsset = Ref.Texture.Resolve();
+		Info.TexelFactor = Ref.TexelFactor * ComponentScale;
+		Info.PackedRelativeBox = PackedRelativeBox_Identity;
+		UnpackRelativeBox(Bounds, Info.PackedRelativeBox, Info.Bounds);
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////
--- Source/VoxelCore/Public/VoxelMinimal/VoxelMaterialRef.h
+++ Source/VoxelCore/Public/VoxelMinimal/VoxelMaterialRef.h
@@ -6,6 +6,16 @@
 #include "VoxelMinimal/VoxelAtomic.h"
 #include "VoxelMinimal/VoxelObjectPtr.h"
 
+class UStreamableRenderAsset;
+class FStreamingTextureLevelContext;
+struct FStreamingRenderAssetPrimitiveInfo;
+
+struct VOXELCORE_API FVoxelTextureStreamingRef
+{
+	TVoxelObjectPtr<UStreamableRenderAsset> Texture;
+	float TexelFactor = 0.f;
+};
+
 // Keeps material references alive
 // This is needed because material objects are re-instantiated in-place when recompiled
 // ie the object pointer is the same but with a new serial number, invalidating any weak pointer to it
@@ -29,12 +39,21 @@ public:
 	{
 		return WeakMaterial;
 	}
+	void GetStreamingRenderAssetInfo(
+		FStreamingTextureLevelContext& LevelContext,
+		const FBoxSphereBounds& Bounds,
+		float ComponentScale,
+		TArray<FStreamingRenderAssetPrimitiveInfo>& OutStreamingRenderAssets);
 
 protected:
 	FVoxelMaterialRef() = default;
 
 	TObjectPtr<UMaterialInterface> Material;
 	TVoxelObjectPtr<UMaterialInterface> WeakMaterial;
+
+	TOptional<EMaterialQualityLevel::Type> QualityLevel;
+	TOptional<ERHIFeatureLevel::Type> FeatureLevel;
+	TVoxelArray<FVoxelTextureStreamingRef> TextureStreamingRefs;
 
 	friend class FVoxelMaterialRefManager;
 };
